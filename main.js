845d64b98b23eb00429674e1f39027d77baa11e4
Oscarfp <oscarfp1992@gmail.com>
2024-09-11 12:34:32 +0200

    creacion bbdd productos

---
+++ main.js
@@ -0,0 +1,114 @@
+const products = [
+  {
+    name: 'Ámsterdam Canal',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/amsterdam.jpg'
+  },
+  {
+    name: 'Forest',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/forest.jpg'
+  },
+  {
+    name: 'Fragments Nº2',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/fragments_2".jpg'
+  },
+  {
+    name: 'Bicycle in front of building',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/bicycle.jpg'
+  },
+  {
+    name: 'Highlands Bull',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/highlands_bull.jpg'
+  },
+  {
+    name: 'Ámsterdam Canal',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/amsterdam.jpg'
+  },
+  {
+    name: 'Madrid Spain coordinates poster',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/madrid_spain.jpg'
+  },
+  {
+    name: 'Le Flamant Rosé',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/le_flamant_rosé.jpg'
+  },
+  {
+    name: 'Roadtrip',
+    price: 15.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/roadtrip.jpg'
+  },
+  {
+    name: 'Majestic Lion',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/majestic_lion.jpg'
+  },
+  {
+    name: 'Malibu Dream',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/malibu_dream.jpg'
+  },
+  {
+    name: 'Michael Jordan',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/roadtrip.jpg'
+  },
+  {
+    name: 'Orange Strokes-Ejaaz Haniff',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/orange_strokes.jpg'
+  },
+  {
+    name: 'Toy Ronaldo',
+    price: 10.95,
+    stars: 4,
+    reviews: 250,
+    seller: 'ImgArt',
+    image: 'assets/images/toy_ronaldo.jpg'
+  }
+]
