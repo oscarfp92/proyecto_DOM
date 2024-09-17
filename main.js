ea6107f15c9378b7661442def395dc8c5368974f
Oscarfp <oscarfp1992@gmail.com>
2024-09-17 13:04:21 +0200

    ok

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ const products = [
     image: 'assets/images/amsterdam.jpg'
   },
   {
-    name: 'Madrid Spain coordinates poster',
+    name: 'Madrid Spain  poster',
     price: 10.95,
     stars: 4,
     reviews: 250,
@@ -124,6 +124,7 @@ header.appendChild(title)
 const day = document.createElement('button')
 day.textContent = '🌙'
 header.appendChild(day)
+day.classList.add ('day')
 
 // Main.
 const page = document.createElement('main')
@@ -183,9 +184,27 @@ listProducts.classList.add('gallery')
 sectionFind.classList.add('categories')
 sectionProducts.classList.add('products')
 
+// Footer
+  const pies = ['Aviso Legal', 'Atención al cliente', 'Sobre nosotros', 'Sostenibilidad', 'Términos y condiciones']
+  const footer = document.createElement ('footer');
+  const ulFooter = document.createElement ('ul')
+  
+  
+  for (const pie of pies) {
+  const liFooter = document.createElement ('li');
+  liFooter.textContent = pie
+  ulFooter.appendChild (liFooter)
+}
+
+
+footer.appendChild (ulFooter)
+page.appendChild (footer)
+
+
+
 // Añado eventos.
 
-//Modo noche
+// Modo noche
 const pictures = document.querySelectorAll (".card")
 day.addEventListener ("click", () => {
   document.body.classList.toggle ("dark");
@@ -197,8 +216,54 @@ day.addEventListener ("click", () => {
   }
 })
 
-//Zoom articulos
+const menus = document.querySelectorAll ("a");
+day.addEventListener ("click", () => {
+  for (const menu of menus){
+  menu.classList.toggle ("dark");
+}})
+
+const h1 = document.querySelector ("h1");
+day.addEventListener ("click", () => {
+  h1.classList.toggle ("dark");
+})
+
+const pie = document.querySelector ("footer");
+day.addEventListener ("click", () => {
+  footer.classList.toggle ("dark");
+})
+
+
+// Efecto botón fav
 const favoritos = document.querySelectorAll (".fav")
 for (const favorito of favoritos) {
 favorito.addEventListener ("click", () => {
-    favorito.classList.toggle ("favzoom")})}
+    favorito.classList.toggle ("favzoom")})};
+
+const carros = document.querySelectorAll (".buy")
+for (const carro of carros) {
+  carro.addEventListener ("click", () => 
+  carro.classList.toggle ("favzoom"))
+}
+
+
+// Ampliación de imágenes
+const div = document.createElement('div');
+div.classList.add ('zoomimage'); 
+document.body.appendChild(div);
+
+const imageLarge = document.createElement('img');
+div.appendChild(imageLarge);
+
+const images = document.querySelectorAll (".image")
+for (const image of images) {
+  image.addEventListener("click", () => {
+  imageLarge.src = image.src;
+    div.classList.add("visible");
+  });
+}
+
+div.addEventListener("click", (e) => {
+  if (e.target !== imageLarge) {
+    div.classList.remove("visible");
+  }
+});
